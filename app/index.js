cb1daca967480944af61303e7a66b3b876aaccbf
Jadenz <iamfoxuriel@gmail.com>
2017-02-23 22:29:12 +0800

    fix eslint

--- app/index.js
+++ app/index.js
@@ -1,9 +1,8 @@
-import 'babel-polyfill'
-import React from 'react'
-import ReactDOM from 'react-dom'
-
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-    <h1>Hello SMBO!</h1>,
-    document.body
+  <h1>Hello SMBO!</h1>,
+  document.body
 );
