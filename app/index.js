c42f24ab978f464e3a89eaa93a28ffdc4fb9c337
Jaden Zhuang <Jaden.Zhuang@ehealth-china.com>
2017-02-24 17:20:45 +0800

    prepare render container

--- app/index.js
+++ app/index.js
@@ -4,8 +4,13 @@ import ReactDOM from 'react-dom';
 import '../ui/less/index.less';
 
 
-// Render the React application to the DOM
+// prepare render container
+const rootDiv = document.createElement('div');
+rootDiv.id = 'root';
+document.body.appendChild(rootDiv);
+
 ReactDOM.render(
   <h1 className="red">Hello SMBO!</h1>,
-  document.body
+  document.getElementById('root'),
+  () => { console.log('Initial done'); }
 );
